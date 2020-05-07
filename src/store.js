e900085c299f5e6afb029a406274221a7eeb5af4
Rochelle Palma <palmarochelleg@gmail.com>
2020-05-07 00:29:42 -0700

    added sample data

---
+++ src/store.js
@@ -0,0 +1,17 @@
+const bookmarks = [{ 
+        id: 1,
+        title: 'Youtube',
+        url: 'https://www.youtube.com',
+        description: 'Popular video streaming site',
+        rating: 4.5
+    },
+    { 
+        id: 2,
+        title: 'Netflix',
+        url: 'https://www.netflix.com',
+        description: 'Another popular video streaming site',
+        rating: 5
+    },
+]
+
+module.exports =  { bookmarks }
