ef651a76bc5e03cf25ddb2e6549290ed5db73593
Rochelle Palma <palmarochelleg@gmail.com>
2020-05-06 23:22:57 -0700

    added route handlers

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -2,93 +2,98 @@ const express = require('express')
 const { v4: uuid } = require('uuid')
 const logger = require('../logger')
 const { bookmarks } = require('./store')
-const url = require('url');
+const validator = require('validator');
 
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
 
 bookmarksRouter
-  .route('/bookmarks')
-  .get((req, res) => {
-    res.json(bookmarks);
-  })
+    .route('/bookmarks')
+    .get((req, res) => {
+        res.json(bookmarks);
+    })
 
-  .post(bodyParser, (req, res) => {
-    const { title, url, description='', rating=1 } = req.body;
-      
-    if (!title) {
-      logger.error(`Title is required`);
-      return res
-        .status(400)
-        .send('Invalid data');
-    }
+    .post(bodyParser, (req, res) => {
+        const { title, url, description='', rating=1 } = req.body;
+        
+        if (!title) {
+            logger.error('Title is required');
+            return res
+                .status(400)
+                .send('Invalid data');
+        }
 
-    if (!url) {
-        logger.error(`URL is required`);
-        return res
-          .status(400)
-          .send('Invalid data');
-    }
+        if (!url) {
+            logger.error('URL is required');
+            return res
+            .status(400)
+            .send('Invalid data');
+        }
 
-    if (Number.isNaN(parseFloat(rating))) {
-        logger.error(`Rating is not number`);
-        return res
-          .status(400)
-          .send('Invalid data');
-    }
-  
+        if (!validator.isURL(url)){
+            logger.error('URL is not valid');
+            return res
+            .status(400)
+            .send('Invalid data');
+        } 
+        
+        if (Number.isNaN(parseFloat(rating))) {
+            logger.error(`Rating is not number`);
+            return res
+            .status(400)
+            .send('Invalid data');
+        }
     
-  
-    // get an id
-    const id = uuid();
+        const id = uuid();
 
-    const bookmark = {
-      id,
-      title,
-      url,
-      description,
-      rating
-    };
-  
-    lists.push(list);
-  
-    logger.info(`List with id ${id} created`);
-  
-    res
-      .status(201)
-      .location(`http://localhost:8000/list/${id}`)
-      .json({id});
-  })
+        const bookmark = {
+            id,
+            title,
+            url,
+            description,
+            rating
+        };
+    
+        bookmarks.push(bookmark);
+    
+        logger.info(`Bookmark with id ${id} created`);
+    
+        res
+            .status(201)
+            .location(`http://localhost:8000/bookmarks/${id}`)
+            .json({bookmark});
+    })
 
 bookmarksRouter
-  .route('/bookmarks/:id')
-  .get((req, res) => {
-    const { id } = req.params;
-    const bookmark = bookmarks.find(bookmark => bookmark.id == id);
-  
-    if (!bookmark) {
-      logger.error(`Bookmark with id ${id} not found.`);
-      return res
-        .status(404)
-        .send('Bookmark Not Found');
-    }
-    res.json(bookmark);
-  })
+    .route('/bookmarks/:id')
+    .get((req, res) => {
+        const { id } = req.params;
+        const bookmark = bookmarks.find(bookmark => bookmark.id == id);
+    
+        if (!bookmark) {
+        logger.error(`Bookmark with id ${id} not found.`);
+        return res
+            .status(404)
+            .send('Bookmark Not Found');
+        }
+
+        res.json(bookmark);
+    })
   .delete((req, res) => {
     const { id } = req.params;
   
-    const listIndex = lists.findIndex(li => li.id == id);
+    const bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id == id);
   
-    if (listIndex === -1) {
-      logger.error(`List with id ${id} not found.`);
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
       return res
         .status(404)
         .send('Not Found');
     }
   
-    lists.splice(listIndex, 1);
+    bookmarks.splice(bookmarkIndex, 1);
   
-    logger.info(`List with id ${id} deleted.`);
+    logger.info(`Bookmark with id ${id} deleted.`);
     res
       .status(204)
       .end();
